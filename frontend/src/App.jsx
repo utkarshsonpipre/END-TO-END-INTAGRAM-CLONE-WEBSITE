3fc38cb11de678f9d781fbb8a49b02c352d8790d
utkarshsonpipre@gmail.com <utkarshsonpipre@gmail.com>
2025-06-25 15:40:23 +0530

    deployed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -55,7 +55,7 @@ function App() {
 
   useEffect(() => {
     if (user) {
-      const socketio = io('http://localhost:8000', {
+      const socketio = io('https://end-to-end-intagram-clone-website.onrender.com', {
         query: {
           userId: user?._id
         },
