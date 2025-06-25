949bd33f2604623e32b18661e577205aa5866968
utkarshsonpipre@gmail.com <utkarshsonpipre@gmail.com>
2025-06-25 17:23:06 +0530

    done

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -65,9 +65,9 @@ const Profile = () => {
               <div className='flex flex-col gap-1'>
                 <span className='font-semibold'>{userProfile?.bio || 'bio here...'}</span>
                 <Badge className='w-fit' variant='secondary'><AtSign /> <span className='pl-1'>{userProfile?.username}</span> </Badge>
-                <span>🤯Learn code with patel mernstack style</span>
-                <span>🤯Turing code into fun</span>
-                <span>🤯DM for collaboration</span>
+                <span>🧑‍💻Hey its me utkarsh sonpipre</span>
+                <span>💻 Building cool stuff with code </span>
+                <span>📈 Automating trades, one algo at a time</span>
               </div>
             </div>
           </section>
